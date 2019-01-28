b81f004a0935ce89ce40fbaa4651720659457883
Osvaldo Larancuent <osvaldo.larancuent@gmail.com>
2019-01-28 07:24:38 -0400

    Modificar interfaz para que opere in Spanish es-DO

--- mi_encuesta/settings.py
+++ mi_encuesta/settings.py
@@ -103,7 +103,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-DO'
 
 TIME_ZONE = 'UTC'
 
